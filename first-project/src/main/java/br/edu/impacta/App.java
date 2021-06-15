bf7268a68590e54e7c1955ea5b4cc04ec0db3edf
EricEOL <lopes.eric051@gmail.com>
2021-06-15 20:58:26 -0300

    Utilizando scope singleton

--- first-project/src/main/java/br/edu/impacta/App.java
+++ first-project/src/main/java/br/edu/impacta/App.java
@@ -1,6 +1,10 @@
 package br.edu.impacta;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+
+import br.edu.impacta.services.abstracts.StudentService;
+import br.edu.impacta.services.impl.StudentServiceImpl;
 
 /**
  * Hello world!
@@ -8,10 +12,27 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  */
 public class App 
 {
+    private static void runService(GenericApplicationContext ctx) {
+        var svc = ctx.getBean(StudentService.class);
+        var students = svc.findAll();
+        students.forEach(System.out::println);
+    }
+
+    private static void runServiceTwo(GenericApplicationContext ctx) {
+        var svc = ctx.getBean(StudentService.class);
+        var students = svc.findAll();
+        students.forEach(System.out::println);
+    }
+
     public static void main( String[] args )
     {
         var appContext = new AnnotationConfigApplicationContext(AppConfig.class);
         appContext.registerShutdownHook();
         System.out.println("spring working...");
+
+        runService(appContext);
+        runServiceTwo(appContext);
+
+        appContext.close();
     }
 }
---
+++ first-project/src/main/java/br/edu/impacta/services/impl/StudentServiceImpl.java
@@ -0,0 +1,25 @@
+package br.edu.impacta.services.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import br.edu.impacta.models.Student;
+import br.edu.impacta.services.abstracts.StudentService;
+
+@Service
+@Scope(BeanDefinition.SCOPE_SINGLETON)
+public class StudentServiceImpl implements StudentService {
+
+    public StudentServiceImpl() {
+      super();
+      System.out.println("Creating new StudentServiceImpl...");
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return List.of(new Student("Emilio"));
+    }
+}
